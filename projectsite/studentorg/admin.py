9dd5539268db7a6cfbc000ccabd747a4cb1ddd00
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-02 19:01:55 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname")
+
+
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+
+    search_fields = ("student__lastname", "student__firstname")
+
+    def get_member_program(self, obj):
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+
+        except Student.DoesNotExist:
+            return None
