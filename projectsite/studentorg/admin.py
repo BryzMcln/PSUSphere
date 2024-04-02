de6a9183bb23fe74195e1a6854385e919f303a06
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-02 10:35:20 +0800

    models and admin working properly

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import College, Program, Organization, Student, OrgMember
+
+admin.site.register(College)
+admin.site.register(Program)
+admin.site.register(Organization)
+admin.site.register(Student)
+admin.site.register(OrgMember)
--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add = True, db_index = True)
+    update_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        abstract = True
+
+class College(BaseModel):
+    college_name = models.CharField(max_length = 150)
+    def __str__(self):
+        return self.college_name
+
+class Program(BaseModel):
+    prog_name = models.CharField(max_length = 150)
+    college = models.ForeignKey(College, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.prog_name
+    
+class Organization(BaseModel):
+    name = models.CharField(max_length = 250)
+    college = models.ForeignKey(College, null = True, blank = True, on_delete = models.CASCADE)
+    description = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return self.name
+class Student(BaseModel):
+    student_id = models.CharField(max_length = 15)
+    firstname = models.CharField(max_length = 25)
+    lastname = models.CharField(max_length = 25)
+    middlename = models.CharField(max_length = 25, blank = True, null = True)
+    program = models.ForeignKey(Program, on_delete = models.CASCADE)
+    def __str__(self):
+        return f"{self.lastname}, {self.firstname}"
+    
+class OrgMember(BaseModel):
+    student = models.ForeignKey(Student, on_delete = models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete = models.CASCADE)
+    date_joined = models.DateField()
+    
+
+
+    
+
+
