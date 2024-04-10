e73883fda3cdde6c568621369fb98baf8eb773ca
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-10 20:26:26 +0800

    organization add and edit

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
-    path('organization_list', OrganizationList.as_view(), name='organization-list')
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization_update'),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization
@@ -12,3 +15,16 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_add.html'
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_edit.html'
+    success_url = reverse_lazy('organization-list')
+
