8a90cedd1a39891ce9456b8397671e66aa9cc516
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-10 18:56:02 +0800

    organizational list

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView
+from studentorg.views import HomePageView, OrganizationList
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    path('organization_list', OrganizationList.as_view(), name='organization-list')
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -7,4 +7,8 @@ class HomePageView(ListView):
     context_object_name = 'home'
     template_name = "home.html"
 
-# Create your views here.
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
