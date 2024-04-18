d75ba5083b1b2bffeb977303416e3b2a6aa3707e
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-18 23:17:16 +0800

    modified urls

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg.views import *
 from studentorg import views
 from django.contrib.auth import views as auth_views
 
@@ -25,8 +25,24 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
-    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization_update'),
-    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization_delete'),
-    re_path(r'^login/$', auth_views.LoginView.as_view( template_name='login.html'), name='login'),
+    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    path('college_list', CollegeList.as_view(), name='college-list'),
+    path('college_list/add', CollegeCreateView.as_view(), name='college-add'),
+    path('college_list/<pk>', CollegeUpdateView.as_view(), name='college-update'),
+    path('college_list/<pk>/delete', CollegeDeleteView.as_view(), name='college-delete'),
+    path('student_list', StudentList.as_view(), name='student-list'),
+    path('student_list/add', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<pk>', StudentUpdateView.as_view(), name='student-update'),
+    path('student_list/<pk>/delete', StudentDeleteView.as_view(), name='student-delete'),
+    path('orgmem_list', OrgMemList.as_view(), name='orgmem-list'),
+    path('orgmem_list/add', OrgMemCreateView.as_view(), name='orgmem-add'),
+    path('orgmem_list/<pk>', OrgMemUpdateView.as_view(), name='orgmem-update'),
+    path('orgmem_list/<pk>/delete', OrgMemDeleteView.as_view(), name='orgmem-delete'),
+    path('program_list', ProgramList.as_view(), name='program-list'),
+    path('program_list/add', ProgramCreateView.as_view(), name='program-add'),
+    path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
+    path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
