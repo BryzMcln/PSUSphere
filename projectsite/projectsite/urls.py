a31b900bb188d452ae38ae498f5b4b8ee059d310
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-25 23:57:01 +0800

    successful chart setup

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -23,6 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    path('dashboard-chart', ChartView.as_view(), name='dashboard-chart'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -171,3 +171,13 @@ class OrgMemDeleteView(DeleteView):
     model = OrgMember
     template_name = "orgmem_del.html"
     success_url = reverse_lazy('orgmem-list')
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def get_queryset(self, *args, **kwargs):
+        pass
