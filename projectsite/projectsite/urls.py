f9f7756ce8bd11ff09fcd8c27693aafabe331c67
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-04-29 16:57:59 +0800

    dynamic pie chart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
     path('dashboard-chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -10,6 +10,12 @@ from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+from django.db import connection
+from django.http import JsonResponse
+from django.db.models.functions import ExtractMonth
+
+from django.db.models import Count
+from datetime import datetime
 @method_decorator(login_required, name='dispatch')
 class HomePageView(ListView):
     model = Organization
@@ -181,3 +187,17 @@ class ChartView(ListView):
     
     def get_queryset(self, *args, **kwargs):
         pass
+
+def PieCountbySeverity(request):
+    query = ''' SELECT severity_level, COUNT(*) as count FROM fire_incident GROUP BY severity_level '''
+    data = {}
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+    if rows:
+        # Construct the dictionary with severity level as keys and count as values
+        data = {severity: count for severity, count in rows}
+    else:
+        data = {}
+    return JsonResponse(data)
