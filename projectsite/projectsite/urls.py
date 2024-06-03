243383ff1d017b65f255cb2f53877bbafff1a6ae
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-06-03 23:51:21 +0800

    added comments on urls

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,48 +1,53 @@
-"""
-URL configuration for projectsite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, re_path
-from studentorg.views import *
-from studentorg import views
+from studentorg.views import (
+    HomePageView, OrganizationList, OrganizationCreateView, 
+    OrganizationUpdateView, OrganizationDeleteView, CollegeList, 
+    CollegeCreateView, CollegeUpdateView, CollegeDeleteView, StudentList, 
+    StudentCreateView, StudentUpdateView, StudentDeleteView, ProgramList, 
+    ProgramCreateView, ProgramUpdateView, ProgramDeleteView, OrgMemList, 
+    OrgMemCreateView, OrgMemUpdateView, OrgMemDeleteView
+)
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Admin URL
     path("admin/", admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
+    
+    # Home page
+    path('', HomePageView.as_view(), name='home'),
+    
+    # Organization URLs
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    
+    # College URLs
     path('college_list', CollegeList.as_view(), name='colleges-list'),
     path('college_list/add', CollegeCreateView.as_view(), name='college-add'),
     path('college_list/<pk>', CollegeUpdateView.as_view(), name='college-update'),
     path('college_list/<pk>/delete', CollegeDeleteView.as_view(), name='college-delete'),
+    
+    # Student URLs
     path('student_list', StudentList.as_view(), name='students-list'),
     path('student_list/add', StudentCreateView.as_view(), name='student-add'),
     path('student_list/<pk>', StudentUpdateView.as_view(), name='student-update'),
     path('student_list/<pk>/delete', StudentDeleteView.as_view(), name='student-delete'),
+    
+    # Organization Member URLs
     path('orgmem_list', OrgMemList.as_view(), name='orgmem-list'),
     path('orgmem_list/add', OrgMemCreateView.as_view(), name='orgmem-add'),
     path('orgmem_list/<pk>', OrgMemUpdateView.as_view(), name='orgmem-update'),
     path('orgmem_list/<pk>/delete', OrgMemDeleteView.as_view(), name='orgmem-delete'),
+    
+    # Program URLs
     path('program_list', ProgramList.as_view(), name='program-list'),
     path('program_list/add', ProgramCreateView.as_view(), name='program-add'),
     path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
     path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
+    
+    # Authentication URLs
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
